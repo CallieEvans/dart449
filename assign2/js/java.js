3ff083c19192fe5863bc9674ed1e80f910f34cee
CallieEvans <datperson23@gmail.com>
2025-01-27 16:52:46 -0500

    finished inclass exercise

--- assign2/js/java.js
+++ assign2/js/java.js
@@ -1,1 +1,25 @@
-alert('hello word');
+
+const changeText = document.querySelector('.text');
+const btn = document.querySelector('.changer');
+
+const day = document.querySelector('#day');
+const night = document.querySelector('#night');
+
+btn.addEventListener('click', function () {
+    changeText.textContent = 'You changed the text';
+});
+
+
+day.addEventListener('click', function () {
+    //This. is calling on the day variable
+    this.classList.add('change');
+    // this.style.opacity = '.2';
+});
+day.addEventListener('dblclick', function () {
+    this.classList.remove('change');
+});
+night.addEventListener('click', function () {
+    //toggle to make it on / off
+    this.classList.toggle('change-night');
+});
+
